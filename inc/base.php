049b0af90cd83f9300ba68abf15156a699135a93
Javier Fuentes <javier.fuentes@amphora.es>
2015-02-13 18:02:54 +0100

    Autoloader namespaces and backslash fix

--- inc/base.php
+++ inc/base.php
@@ -3,5 +3,5 @@
 error_reporting(E_ALL);
 
 spl_autoload_register(function ($class) {
-    require_once(__DIR__ . '/../vendor/' . $class . '.class.php');
+    require_once(__DIR__ . '/../vendor/' . str_replace('\\', '/', $class) . '.class.php');
 });
